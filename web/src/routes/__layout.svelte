cb6aad3e0a8167adffc369882e625b096d9d71c2
wighawag <wighawag@gmail.com>
2021-05-26 19:07:44 +0100

    handle metatags in svelte

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -8,10 +8,29 @@
   import NewVersionNotification from '$lib/components/NewVersionNotification.svelte';
   // import Install from './components/Install.svelte';
   import DarkSwitch from '$lib/components/DarkSwitch.svelte';
+
+  import {appDescription, url as appUrl} from '../../application.json';
+
+  const title = 'Jolly Roger, Template for Decentralised Applications.';
+  const description = appDescription;
+  const host = appUrl.endsWith('/') ? appUrl : appUrl + '/';
+  const previewImage = host + 'preview.png';
 </script>
 
 <svelte:head>
-  <title>Jolly Roger, Template for Decentralised Applications.</title>
+  <title>{title}</title>
+  <meta name="title" content={title} />
+  <meta name="description" content={description} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={host} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={previewImage} />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content={host} />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={description} />
+  <meta property="twitter:image" content={previewImage} />
 </svelte:head>
 
 <NoInstallPrompt />
