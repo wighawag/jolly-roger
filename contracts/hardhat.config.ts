dbfa0352609e0fc1bf5c09b9d4891de249f57a97
wighawag <wighawag@gmail.com>
2021-10-16 09:10:40 +0100

    update from template-ethereum

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -7,6 +7,11 @@ import '@typechain/hardhat';
 import 'solidity-coverage';
 import {node_url, accounts} from './utils/network';
 
+// While waiting for hardhat PR: https://github.com/nomiclabs/hardhat/pull/1542
+if (process.env.HARDHAT_FORK) {
+  process.env['HARDHAT_DEPLOY_FORK'] = process.env.HARDHAT_FORK;
+}
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -27,11 +32,13 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
+      initialBaseFeePerGas: 0, // to fix : https://github.com/sc-forks/solidity-coverage/issues/652, see https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136
       // process.env.HARDHAT_FORK will specify the network that the fork is made from.
       // this line ensure the use of the corresponding accounts
       accounts: accounts(process.env.HARDHAT_FORK),
       forking: process.env.HARDHAT_FORK
         ? {
+            // TODO once PR merged : network: process.env.HARDHAT_FORK,
             url: node_url(process.env.HARDHAT_FORK),
             blockNumber: process.env.HARDHAT_FORK_NUMBER ? parseInt(process.env.HARDHAT_FORK_NUMBER) : undefined,
           }
