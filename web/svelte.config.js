b31ef5d2be9db65d0b5eb13cd22f3c91dfc3e9aa
wighawag <wighawag@gmail.com>
2021-12-17 11:18:39 +0000

    VERCEL public folder + VIT_CHAIN_ID

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,15 @@
 import preprocess from 'svelte-preprocess';
 import adapter_ipfs from 'sveltejs-adapter-ipfs';
+import fs from 'fs';
+
+if (!process.env.VITE_CHAIN_ID) {
+  try {
+    const contractsInfo = JSON.parse(fs.readFileSync('./src/lib/contracts.json'));
+    process.env.VITE_CHAIN_ID = contractsInfo.chainId;
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +17,8 @@ const config = {
 
   kit: {
     adapter: adapter_ipfs({
+      assets: process.env.VERCEL ? '../public' : './build', // allow no config when creating a vercel project
+      pages: process.env.VERCEL ? '../public' : './build',
       removeBuiltInServiceWorkerRegistration: true,
       injectPagesInServiceWorker: true,
       injectDebugConsole: true,
