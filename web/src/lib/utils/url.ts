c712b7b808af60a4ac63c67eaaaf8a4cf5aed4ae
wighawag <wighawag@gmail.com>
2022-01-02 11:54:02 +0000

    fix

--- web/src/lib/utils/url.ts
+++ web/src/lib/utils/url.ts
@@ -15,12 +15,11 @@ export function url(path: string, hash?: string): string {
 export function urlOfPath(url: string, path: string): boolean {
   // console.log({url, path, base});
 
-  // hmm, svelte-kit has changed its path handling, TODO : deal with it in svelte-kit-ipfs-adapter
-  if (path.startsWith(base)) {
-    path = path.slice(base.length);
-  } else if (path.startsWith('//prerender')) {
-    // hmm, svelte-kit is prepending "//prerender" when using adapter-static
+  // hmm, svelte-kit is prepending "//prerender" when using adapter-static
+  if (path.startsWith('//prerender')) {
     path = path.slice(11);
+  } else if (path.startsWith(base)) { // hmm, svelte-kit has changed its path handling, TODO : deal with it in svelte-kit-ipfs-adapter
+    path = path.slice(base.length);
   }
 
   const basicUrl = url.split('?')[0].split('#')[0];
