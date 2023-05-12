3d2da1ffa70de1df023b069f241e6157b6ebceb4
wighawag <wighawag@gmail.com>
2023-05-12 17:22:45 +0100

    remove jolly-roger theme

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -14,42 +14,6 @@ const config = {
 		logs: false,
 		darkTheme: 'dracula',
 		themes: [
-			{
-				'jolly-roger': {
-					// fontFamily:
-					// 	'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
-
-					primary: '#ec4899',
-
-					secondary: '#d946ef',
-
-					accent: '#f9208d',
-
-					neutral: '#263140',
-
-					'base-100': '#FFFFFF',
-					'base-200': '#FFEEEE',
-
-					info: '#487BDB',
-
-					success: '#2EE5BA',
-
-					warning: '#EC8F0E',
-
-					error: '#FB284B',
-
-					'--rounded-box': '1rem', // border radius rounded-box utility class, used in card and other large boxes
-					'--rounded-btn': '0.5rem', // border radius rounded-btn utility class, used in buttons and similar element
-					'--rounded-badge': '1.9rem', // border radius rounded-badge utility class, used in badges and similar
-					'--animation-btn': '0.25s', // duration of animation when you click on button
-					'--animation-input': '0.2s', // duration of animation for inputs like checkbox, toggle, radio, etc
-					'--btn-text-case': 'uppercase', // set default text transform for buttons
-					'--btn-focus-scale': '0.95', // scale transform of button when you focus on it
-					'--border-btn': '1px', // border width of buttons
-					'--tab-border': '1px', // border width of tabs
-					'--tab-radius': '0.5rem', // border radius of tabs
-				},
-			},
 			'light',
 			'dark',
 			'cupcake',
