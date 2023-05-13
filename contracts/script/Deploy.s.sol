6c0902550d8bd3041d3cf74c0e30f6d3359a8c7f
wighawag <wighawag@gmail.com>
2023-05-13 14:49:37 +0100

    latest forge-deploy-proxy

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {DeployScript, Deployer} from "forge-deploy/DeployScript.sol";
-import {ProxiedDeployerFunctions, ProxiedDeployOptions} from "generated/deployer/ProxiedDeployerFunctions.g.sol";
+import {ProxiedDeployerFunctions, ProxyOptionsOnTag} from "generated/deployer/ProxiedDeployerFunctions.g.sol";
 import {GreetingsRegistry} from "src/GreetingsRegistry.sol";
 
 contract Deployments is DeployScript {
@@ -10,7 +10,7 @@ contract Deployments is DeployScript {
 
     function deploy() external returns (GreetingsRegistry) {
         return deployer.deploy_GreetingsRegistry(
-            "Registry", "", ProxiedDeployOptions({proxyOnTag: "testnet", proxyOwner: vm.envAddress("DEPLOYER")})
+            "Registry", "", ProxyOptionsOnTag({onTag: "testnet", owner: vm.envAddress("DEPLOYER")})
         );
     }
 }
