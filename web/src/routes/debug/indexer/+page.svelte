69692836fa04e1dd56dcbdb35579d446b66c1cae
wighawag <wighawag@gmail.com>
2023-05-18 05:20:43 +0100

    better indexer state display

--- web/src/routes/debug/indexer/+page.svelte
+++ web/src/routes/debug/indexer/+page.svelte
@@ -18,31 +18,44 @@
 	$: stateDisplayed = $state && addLengthToFields($state);
 </script>
 
-<progress value={($syncing.lastSync?.syncPercentage || 0) / 100} style="width:100%;" />
+<div
+	class={`radial-progress m-2 ${
+		$syncing.loading ? 'text-green-400' : $status.state === 'Idle' ? 'text-red-400' : 'text-green-400'
+	}`}
+	style={`--value:${$syncing.lastSync?.syncPercentage || 0};`}
+>
+	{$syncing.lastSync?.syncPercentage
+		? $syncing.lastSync?.syncPercentage === 100
+			? $status.state
+			: `${$syncing.lastSync?.syncPercentage}%`
+		: '0%'}
+</div>
 
-<p>status: {$status.state}</p>
-<p>loading: {$syncing.loading}</p>
-<p>catchingUp: {$syncing.catchingUp}</p>
-<p>autoIndexing: {$syncing.autoIndexing}</p>
-<p>fetchingLogs: {$syncing.fetchingLogs}</p>
-<p>processingFetchedLogs: {$syncing.processingFetchedLogs}</p>
+<!-- <progress value={($syncing.lastSync?.syncPercentage || 0) / 100} style="width:100%;" /> -->
+
+<p class="m-1">status: {$status.state}</p>
+<p class="m-1">catchingUp: {$syncing.catchingUp}</p>
+<p class="m-1">autoIndexing: {$syncing.autoIndexing}</p>
+<p class="m-1">fetchingLogs: {$syncing.fetchingLogs}</p>
+<p class="m-1">processingFetchedLogs: {$syncing.processingFetchedLogs}</p>
 
 {#if $syncing.numRequests !== undefined}
-	<p>requests sent: {$syncing.numRequests}</p>
+	<p class="m-1">requests sent: {$syncing.numRequests}</p>
 {/if}
-<p>
+<p class="m-1">
 	block processed: {$syncing.lastSync?.numBlocksProcessedSoFar?.toLocaleString() || 0}
 </p>
 
-<p>
+<p class="m-1">
 	latestBlock: {$syncing.lastSync?.latestBlock || 0}
 </p>
 
+<div class="mt-4 text-center w-full">State</div>
+
 {#if $state}
-	{JSON.stringify(
-		stateDisplayed,
-		(key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
-	)}
+	<code class="m-2 block bg-base-300">
+		{JSON.stringify(stateDisplayed, (key, value) => (typeof value === 'bigint' ? value.toString() : value), 2)}
+	</code>
 {:else}
 	{JSON.stringify($syncing)}
 {/if}
