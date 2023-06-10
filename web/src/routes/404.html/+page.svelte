7095cc6af7174f5abd33b05d03195a7e5d6b6959
wighawag <wighawag@gmail.com>
2023-06-10 07:41:28 +0100

    some todo for 404

--- web/src/routes/404.html/+page.svelte
+++ web/src/routes/404.html/+page.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
 	import {page} from '$app/stores';
-	import {isSameRoute, route} from '$lib/utils/path';
-	import {onMount} from 'svelte';
-	let newURL: string | undefined;
-	onMount(() => {
-		if (!isSameRoute($page.url.pathname, '/404.html/')) {
-			newURL = route(`/404.html/?notfound=${$page.url.pathname}`);
-			location.href = newURL;
-		}
-	});
 </script>
+
+<!-- TODO inject some script tag in the generated build/404.html -->
+<!-- <script src={url('/404.js')} /> -->
+<!-- if (!isSameRoute($page.url.pathname, '/404.html/')) {
+	newURL = route(`/404.html/?notfound=${$page.url.pathname}`);
+	location.href = newURL;
+} -->
 
 <section class="py-2 px-4 text-center">
 	<div class="max-w-auto md:max-w-lg mx-auto">
