20030ab2701457998f667e7fcac0453b0ebc20c1
wighawag <wighawag@gmail.com>
2023-06-10 07:18:41 +0100

    404.html folder

---
+++ web/src/routes/404.html/+page.svelte
@@ -0,0 +1,7 @@
+<section class="py-2 px-4 text-center">
+	<div class="max-w-auto md:max-w-lg mx-auto">
+		<p class="!text-2xl flex justify-center space-x-2 font-black my-8">
+			Could not find the page
+		</p>
+	</div>
+</section>
