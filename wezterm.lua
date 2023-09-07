c1a271a4426ce2d41ae9d5daf6746bf93405ec7d
wighawag <wighawag@gmail.com>
2023-09-07 10:58:09 +0000

    fix wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -11,29 +11,33 @@ wezterm.on('gui-startup', function(cmd)
   tab:set_title 'jolly-roger'
   window:set_title 'jolly-roger'
 
-	local pane_indexer = pane:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm start:indexer; bash'},
+  local pane_indexer = pane:split {
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm indexer:dev; bash'},
     direction = 'Bottom'
   }
 	local pane_web = pane_indexer:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm start:web; bash'},
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm web:dev; bash'},
+    direction = 'Right'
+  }
+  local pane_common = pane_indexer:split {
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm common:dev; bash'},
     direction = 'Right'
   }
 
 
 	local pane_local_node = pane:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm start:local_node; bash'},
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm local_node; bash'},
     direction = 'Bottom'
   }
 
 
   local pane_contracts_compile = pane_local_node:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm start:contracts:compile; bash'},
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm contracts:compile:watch; bash'},
     direction = 'Right'
   }
 
 	local pane_contracts_deploy = pane_contracts_compile:split {
-    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm start:contracts:deploy; bash'},
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm contracts:deploy:watch; bash'},
     direction = 'Bottom'
   }
 
