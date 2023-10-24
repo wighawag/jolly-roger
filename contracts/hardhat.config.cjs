f25e4b161014622a9ba1614015d58aa0b57b6726
wighawag <wighawag@gmail.com>
2023-10-24 22:20:42 +0100

    block_time interval mining via node task

--- contracts/hardhat.config.cjs
+++ contracts/hardhat.config.cjs
@@ -4,6 +4,16 @@ require('@nomicfoundation/hardhat-network-helpers');
 const {addForkConfiguration, addNetworksFromEnv} = require('hardhat-rocketh');
 require('vitest-solidity-coverage/hardhat');
 
+const {task} = require('hardhat/config');
+// we override the node task to set up our block time interval
+// setting it up in the config below would also set it in tests and we do not want that
+task('node').setAction(async (args, hre, runSuper) => {
+	hre.config.networks.hardhat.mining.interval = process.env['BLOCK_TIME']
+		? parseInt(process.env['BLOCK_TIME']) * 1000
+		: undefined;
+	return runSuper(args);
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: '0.8.20',
@@ -14,10 +24,6 @@ module.exports = {
 			addNetworksFromEnv({
 				hardhat: {
 					initialBaseFeePerGas: 0,
-					// TODO not on test
-					// mining: {
-					// 	interval: process.env['BLOCK_TIME'] ? parseInt(process.env['BLOCK_TIME']) * 1000 : undefined,
-					// },
 				},
 			}),
 		),
