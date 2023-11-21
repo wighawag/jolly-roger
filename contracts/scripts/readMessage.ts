835770867b36b57cb1ed19b3e1641d1fad43d2b4
wighawag <wighawag@gmail.com>
2023-11-21 11:33:35 +0000

    execute + connection

--- contracts/scripts/readMessage.ts
+++ contracts/scripts/readMessage.ts
@@ -1,14 +1,14 @@
 import {Deployment, loadEnvironment} from 'rocketh';
 import {context} from '../deploy/_context';
-import {network} from 'hardhat';
+import hre from 'hardhat';
 import {EIP1193ProviderWithoutEvents} from 'eip-1193';
-import {contract} from './viem';
+import {fetchContract} from '../utils/connection';
 
 async function main() {
 	const env = await loadEnvironment(
 		{
-			provider: network.provider as EIP1193ProviderWithoutEvents,
-			networkName: network.name,
+			provider: hre.network.provider as EIP1193ProviderWithoutEvents,
+			networkName: hre.network.name,
 		},
 		context,
 	);
@@ -16,7 +16,7 @@ async function main() {
 	const args = process.argv.slice(2);
 	const account = (args[0] || process.env.ACCOUNT) as `0x${string}`;
 	const Registry = env.deployments.Registry as Deployment<typeof context.artifacts.GreetingsRegistry.abi>;
-	const RegistryContract = contract(Registry);
+	const RegistryContract = await fetchContract(Registry);
 	const message = await RegistryContract.read.messages([account]);
 
 	console.log({account, message});
--- contracts/scripts/setMessage.ts
+++ contracts/scripts/setMessage.ts
@@ -1,24 +1,26 @@
 import {Deployment, loadEnvironment} from 'rocketh';
 import {context} from '../deploy/_context';
-import {network} from 'hardhat';
+import hre from 'hardhat';
 import {EIP1193ProviderWithoutEvents} from 'eip-1193';
-import {contract, walletClient} from './viem';
+import {fetchContract, getConnection} from '../utils/connection';
 
 async function main() {
 	const env = await loadEnvironment(
 		{
-			provider: network.provider as EIP1193ProviderWithoutEvents,
-			networkName: network.name,
+			provider: hre.network.provider as EIP1193ProviderWithoutEvents,
+			networkName: hre.network.name,
 		},
 		context,
 	);
+
+	const {walletClient} = await getConnection();
 
 	const args = process.argv.slice(2);
 	const message = (args[0] || process.env.MESSAGE) as `0x${string}`;
 
 	const [address] = await walletClient.getAddresses();
 	const Registry = env.deployments.Registry as Deployment<typeof context.artifacts.GreetingsRegistry.abi>;
-	const RegistryContract = contract(Registry);
+	const RegistryContract = await fetchContract(Registry);
 	const hash = await RegistryContract.write.setMessage([message, 1], {account: address});
 
 	console.log({hash, account: address});
