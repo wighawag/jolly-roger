15a09e75d0352da6fc8272d301dfd9da10b43958
wighawag <wighawag@gmail.com>
2023-11-26 22:42:37 +0000

    use index.html for 404 _redirects

--- web/src/routes/_redirects/+server.ts
+++ web/src/routes/_redirects/+server.ts
@@ -1,5 +1,5 @@
 export const prerender = true;
 export const GET = async () => {
 	// see: https://docs.ipfs.tech/how-to/websites-on-ipfs/redirects-and-custom-404s/#examples
-	return new Response(`/* /404.html/ 404`);
+	return new Response(`/* /404.html/index.html 404`);
 };
