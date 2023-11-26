36052456ba57d1d4caedba0cfa76394cc52eac38
wighawag <wighawag@gmail.com>
2023-11-26 22:24:36 +0000

    add _redirects

---
+++ web/src/routes/_redirects/+server.ts
@@ -0,0 +1,5 @@
+export const prerender = true;
+export const GET = async () => {
+	// see: https://docs.ipfs.tech/how-to/websites-on-ipfs/redirects-and-custom-404s/#examples
+	return new Response(`/* /404.html 404`);
+};
