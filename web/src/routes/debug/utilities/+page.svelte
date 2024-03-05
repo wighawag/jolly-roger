b8813ed2953b7cdb3c5a95ca38c58b4501dc4062
wighawag <wighawag@gmail.com>
2024-03-05 19:37:50 +0000

    tweaks

--- web/src/routes/debug/utilities/+page.svelte
+++ web/src/routes/debug/utilities/+page.svelte
@@ -4,41 +4,21 @@
 	import {createExecutor, enableAnvilLogging, increaseBlockTime} from '$lib/utils/debug';
 	import DebugWrapper from '../DebugWrapper.svelte';
 
-	let error: any;
-	let state: 'addTime' | undefined;
-
-	async function addTime(numHours: number) {
-		try {
-			state = 'addTime';
-			await increaseBlockTime(numHours * 3600);
-		} catch (err) {
-		} finally {
-			state = undefined;
-		}
-	}
-
 	$: date = new Date($time.timestamp * 1000);
 
 	let hours = 1;
-
-	const execute_increaseBlockTime = createExecutor(increaseBlockTime);
 </script>
 
 <DebugWrapper>
 	<p slot="title">Date/Time: {date.toLocaleDateString() + `  ` + date.toLocaleTimeString()}</p>
 
-	{#if error}
-		{error.message}
-		<button class="error" on:click={() => (error = undefined)}>OK</button>
-	{:else}
-		<Executor func={increaseBlockTime} args={[1 * 3600]}>Add 1 hours</Executor>
-		<Executor func={increaseBlockTime} args={[23 * 3600]}>Add 23 hours</Executor>
-		<form class="add-x-hours">
-			<label for="hours" />
-			<input id="hours" type="number" bind:value={hours} />
-			<Executor func={increaseBlockTime} args={[hours * 3600]}>Add {hours} hours</Executor>
-		</form>
-	{/if}
+	<Executor func={increaseBlockTime} args={[1 * 3600]}>Add 1 hours</Executor>
+	<Executor func={increaseBlockTime} args={[23 * 3600]}>Add 23 hours</Executor>
+	<form class="add-x-hours">
+		<label for="hours" />
+		<input id="hours" type="number" bind:value={hours} />
+		<Executor func={increaseBlockTime} args={[hours * 3600]}>Add {hours} hours</Executor>
+	</form>
 </DebugWrapper>
 
 <!-- <Executor func={enableAnvilLogging}>Enable Anvil Logging</Executor> -->
